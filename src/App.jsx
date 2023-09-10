cdedeba8c13d9d32750f2ccd2a13c2deb8cdf217
peakit <peakit@gmail.com>
2023-09-10 16:23:53 +0530

    Simplified the functional component App

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const title = 'I am React';
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test Hot Module Replacement (HMR)
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Hello, {title}</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+
+    </div>
+  );
 }
 
-export default App
+export default App;
