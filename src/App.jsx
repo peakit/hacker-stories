0328f7561e1e8bfe725d6baf2f873b0fb9266144
peakit <peakit@gmail.com>
2023-09-10 16:27:15 +0530

    Greeting as object

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const title = 'I am React';
+  const welcome = {
+    greeting: 'Hey',
+    title: 'I am React'
+  };
 
   return (
     <div>
-      <h1>Hello, {title}</h1>
+      <h1>{welcome.greeting}, {welcome.title}</h1>
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
