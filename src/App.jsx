cbe835fea721c64ca80d28afb9f9b849c319b458
peakit <peakit@gmail.com>
2023-09-11 11:01:52 +0530

    Displaying books array by looping it using map

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function formMessage(welcomeObj) {
-  return welcomeObj.greeting +', ' + welcomeObj.title;
+  return welcomeObj.greeting + ', ' + welcomeObj.title;
 }
 
 function App() {
@@ -13,6 +13,25 @@ function App() {
     title: 'I am React'
   };
 
+  const books = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 10,
+      objectId: 0
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org',
+      author: 'Dan Abramov. Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectId: 1
+    }
+  ];
+
   return (
     <div>
       <h1>{formMessage(welcome)}</h1>
@@ -20,6 +39,22 @@ function App() {
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
 
+      <hr />
+
+      <ul>
+        {books.map(function (book) {
+          return (
+            <li key={book.objectId}>
+              <span>
+                <a href={book.url}>{book.title} </a>
+              </span>
+              <span>{book.author} </span>
+              <span>{book.num_comments} </span>
+              <span>{book.points}</span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
